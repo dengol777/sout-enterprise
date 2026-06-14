a32c35818d226e412b8b5e4d9e0ca4185cf2e8bb
dengol777 <dengol777@gmail.com>
2026-06-14 17:02:06 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,11 @@
+from .employee import Employee, WorkHistory
+from .workstation import Workstation, CompensationRule
+from .briefing import BriefingRecord, BriefingVideoRecord
+from .kedo import KedoSignatureRecord, EiisExportLog
 
+__all__ = [
+    "Employee", "WorkHistory",
+    "Workstation", "CompensationRule",
+    "BriefingRecord", "BriefingVideoRecord",
+    "KedoSignatureRecord", "EiisExportLog",
+]
