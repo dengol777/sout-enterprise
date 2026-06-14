abd37572e80a1832b5f74c37e56aa7a028613d16
dengol777 <dengol777@gmail.com>
2026-06-14 17:13:38 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,11 +1,8 @@
 from .employee import Employee, WorkHistory
-from .workstation import Workstation, CompensationRule
+from .workstation import Workstation, RiskFactor
 from .briefing import BriefingRecord, BriefingVideoRecord
-from .kedo import KedoSignatureRecord, EiisExportLog
+from .kedo import KedoSignatureRecord
+from .audit import AuditLog
+from .eiis import EiisExportLog
 
-__all__ = [
-    "Employee", "WorkHistory",
-    "Workstation", "CompensationRule",
-    "BriefingRecord", "BriefingVideoRecord",
-    "KedoSignatureRecord", "EiisExportLog",
-]
+# Важно: импорт всех моделей здесь гарантирует, что они будут зарегистрированы в метаданных SQLModel
